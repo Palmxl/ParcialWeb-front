c75cb0717c5557ebb18aec8214b2953f549fe496
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-05-27 17:51:12 -0500

    Service entidad

--- src/app/services/entidad.service.spec.ts
+++ src/app/services/entidad.service.spec.ts
@@ -1,16 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+// src/app/services/entidad.service.ts
 
-import { EntidadService } from './entidad.service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Entidad } from '../models/entidad.model';
 
-describe('EntidadService', () => {
-  let service: EntidadService;
+@Injectable({
+  providedIn: 'root'
+})
+export class EntidadService {
+  private apiUrl = 'http://localhost:8082/api/entidades';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EntidadService);
-  });
+  constructor(private http: HttpClient) {}
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  getEntidades(): Observable<Entidad[]> {
+    return this.http.get<Entidad[]>(this.apiUrl);
+  }
+
+  getEntidadById(id: number): Observable<Entidad> {
+    return this.http.get<Entidad>(`${this.apiUrl}/${id}`);
+  }
+
+  crearEntidad(entidad: Entidad): Observable<Entidad> {
+    return this.http.post<Entidad>(this.apiUrl, entidad);
+  }
+
+  actualizarEntidad(id: number, entidad: Entidad): Observable<Entidad> {
+    return this.http.put<Entidad>(`${this.apiUrl}/${id}`, entidad);
+  }
+
+  eliminarEntidad(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
